cc1ac1b67e5f35e57bba1b7f66810755aba9d886
Chris <chrisheng86@gmail.com>
2021-07-31 12:37:34 +0800

    Update stack_monitorrc.sh

--- operational/scripts/stack_monitorrc.sh
+++ operational/scripts/stack_monitorrc.sh
@@ -1,20 +1,22 @@
 sessionname stack
+#usage screen -c stack_monitor.rc
+
 #hardstatus alwayslastline '%{= .} %-Lw%{= .}%> %n%f %t*%{= .}%+Lw%< %-=%{g}(%{d}%H/%l%{g})'
 caption always "%{= kw}%-w%{= kG}%{+b}[%n %t]%{-b}%{= kw}%+w %=%d %M %0c %{g}%H%{-}"
 termcapinfo rxvt 'hs:ts=\E]2;:fs=\007:ds=\E]2;screen\007'
 setenv PROMPT_COMMAND /bin/true
 screen -t shell bash
 screen -t keystone bash
-"tuff "tail -f /var/log/apache2/keystone.log
+stuff "tail -f /var/log/apache2/keystone.log"
 screen -t keystone-access bash
-"tuff "tail -f /var/log/apache2/keystone_access.log
+stuff "tail -f /var/log/apache2/keystone_access.log"
 screen -t nova-compute bash
-"tuff "tail -f /var/log/nova/nova-compute.log
+stuff "tail -f /var/log/nova/nova-compute.log"
 screen -t nova-conductor bash
-"tuff "tail -f /var/log/nova/nova-conductor.log
+stuff "tail -f /var/log/nova/nova-conductor.log"
 screen -t nova-scheduler bash
-"tuff "tail -f /var/log/nova/nova-scheduler.log
+stuff "tail -f /var/log/nova/nova-scheduler.log"
 screen -t cinder-volume bash
-"tuff "tail -f /var/log/cinder/cinder-volume.log
+stuff "tail -f /var/log/cinder/cinder-volume.log"
 screen -t cinder-scheduler bash
-"tuff "tail -f /var/log/cinder/cinder-scheduler.log
+stuff "tail -f /var/log/cinder/cinder-scheduler.log"
